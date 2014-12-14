7daa9cbf6cb979c7dfd21cc209528ae066f5bc50
Pao <designer.pao@gmail.com>
2014-12-14 14:44:45 +0800

    1. Add first button 2. Add callback func about Toast

--- app/src/main/java/com/wlock/pao/wlock/MainActivity.java
+++ app/src/main/java/com/wlock/pao/wlock/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -12,8 +14,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Button button= (Button)findViewById(R.id.button);
+        button.setOnClickListener(new Button.OnClickListener(){
+
+            public void onClick(View v) {
+
+                Toast.makeText(v.getContext(), "Toast!", Toast.LENGTH_LONG).show();
+
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

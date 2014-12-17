e98bdb28793dde2679d2711a9f6eb8146332c272
Pao <designer.pao@gmail.com>
2014-12-17 23:18:49 +0800

    [Wlock] Add 4 buttons
    Test for 4 buttons and clicklistener

--- app/src/main/java/com/wlock/pao/wlock/GameActivity.java
+++ app/src/main/java/com/wlock/pao/wlock/GameActivity.java
@@ -1,19 +1,52 @@
 package com.wlock.pao.wlock;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class GameActivity extends ActionBarActivity {
+
+    public boolean flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        Button button2 = (Button)findViewById(R.id.button2);
+        Button button3 = (Button)findViewById(R.id.button3);
+        Button button4 = (Button)findViewById(R.id.button4);
+        Button button5 = (Button)findViewById(R.id.button5);
+
+        button2.setOnClickListener(myClickListener);
+        button3.setOnClickListener(myClickListener);
+        button4.setOnClickListener(myClickListener);
+        button5.setOnClickListener(myClickListener);
     }
 
+    private View.OnClickListener myClickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.button2:
+                    //textView1.setText("textview1");
+                    break;
+                case R.id.button3:
+                    //textView2.setText("textview2");
+                    break;
+                case R.id.button4:
+                    //textView2.setText("textview2");
+                    break;
+                case R.id.button5:
+                    //textView2.setText("textview2");
+                    break;
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {

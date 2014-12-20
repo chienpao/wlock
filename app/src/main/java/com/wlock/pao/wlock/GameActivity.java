32351b1620116ed757ec77baa5c36b03dadb3cf1
Pao <designer.pao@gmail.com>
2014-12-20 10:05:41 +0800

    [Wlock] Change Button to array and change init status
    1. Add Random number to setup button init color
    2. Change Button to 2 by 2 array

--- app/src/main/java/com/wlock/pao/wlock/GameActivity.java
+++ app/src/main/java/com/wlock/pao/wlock/GameActivity.java
@@ -5,6 +5,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,21 +15,22 @@ import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
-
 public class GameActivity extends ActionBarActivity {
 
     boolean flag;
     public Timer timer;
     public TimerTask timerTask;
-    final Handler timerhandler = new Handler();
+    final Handler timerHandler = new Handler();
     public int randomNumber;
     public Random random;
     public MediaPlayer mediaPlayer;
 
-    private Button button2;
-    private Button button3;
-    private Button button4;
-    private Button button5;
+    // 2 by 2 array button
+    private Button[][] button = new Button[2][2];
+
+    private Random rainbowRandom;
+    private int rainbowRandomNumber;
+    private int[][] rainbowArr = new int[2][2];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +38,18 @@ public class GameActivity extends ActionBarActivity {
         setContentView(R.layout.activity_game);
         randomNumber = 0;
         flag = false;
-        mediaPlayer = MediaPlayer.create(this, R.raw.dog);
 
-        button2 = (Button)findViewById(R.id.button2);
-        button3 = (Button)findViewById(R.id.button3);
-        button4 = (Button)findViewById(R.id.button4);
-        button5 = (Button)findViewById(R.id.button5);
-        button2.setEnabled(false);
-        button3.setEnabled(false);
-        button4.setEnabled(false);
-        button5.setEnabled(false);
-        button2.setOnClickListener(myClickListener);
-        button3.setOnClickListener(myClickListener);
-        button4.setOnClickListener(myClickListener);
-        button5.setOnClickListener(myClickListener);
+        button[0][0] = (Button)findViewById(R.id.button2);
+        button[0][1] = (Button)findViewById(R.id.button3);
+        button[1][0] = (Button)findViewById(R.id.button4);
+        button[1][1] = (Button)findViewById(R.id.button5);
+
+        initButtonColorAndStatus();
+
+        button[0][0].setOnClickListener(myClickListener);
+        button[0][1].setOnClickListener(myClickListener);
+        button[1][0].setOnClickListener(myClickListener);
+        button[1][1].setOnClickListener(myClickListener);
         StartRandomAndTimer();
     }
 
@@ -57,29 +57,74 @@ public class GameActivity extends ActionBarActivity {
         public void onClick(View v) {
             switch(v.getId()){
                 case R.id.button2:
-                    button2.setBackgroundColor(Color.WHITE);
+                    button[0][0].setBackgroundColor(Color.WHITE);
                     mediaPlayer.start();
-                    button2.setEnabled(false);
+                    button[0][0].setEnabled(false);
                     break;
                 case R.id.button3:
-                    button3.setBackgroundColor(Color.WHITE);
+                    button[0][1].setBackgroundColor(Color.WHITE);
                     mediaPlayer.start();
-                    button3.setEnabled(false);
+                    button[0][1].setEnabled(false);
                     break;
                 case R.id.button4:
-                    button4.setBackgroundColor(Color.WHITE);
+                    button[1][0].setBackgroundColor(Color.WHITE);
                     mediaPlayer.start();
-                    button4.setEnabled(false);
+                    button[1][0].setEnabled(false);
                     break;
                 case R.id.button5:
-                    button5.setBackgroundColor(Color.WHITE);
+                    button[1][1].setBackgroundColor(Color.WHITE);
                     mediaPlayer.start();
-                    button5.setEnabled(false);
+                    button[1][1].setEnabled(false);
                     break;
             }
 
         }
     };
+
+    void initButtonColorAndStatus() {
+        // Make Random number for init button color
+        rainbowRandomNumber = 0;
+        rainbowRandom = new Random();
+        mediaPlayer = MediaPlayer.create(this, R.raw.dog);
+        int Color;
+
+        for (int i = 0; i < rainbowArr.length; i++) {
+            for (int j = 0; j < rainbowArr[i].length; j++){
+                rainbowRandomNumber = 1 + rainbowRandom.nextInt(7);
+                rainbowArr[i][j] = rainbowRandomNumber;
+                Log.i("Pao", Integer.toString(i) + "," + Integer.toString(j) + "= " + Integer.toString(rainbowRandomNumber));
+                Color = transferNumber2Color();
+                button[i][j].setBackgroundColor(Color);
+            }
+        }
+
+        // the button is disable in init
+        button[0][0].setEnabled(false);
+        button[0][1].setEnabled(false);
+        button[1][0].setEnabled(false);
+        button[1][1].setEnabled(false);
+    }
+
+    int transferNumber2Color(){
+        switch(rainbowRandomNumber){
+            case 1:
+                return Color.RED;
+            case 2:
+                return Color.argb(48, 255, 205, 0);
+            case 3:
+                return Color.YELLOW;
+            case 4:
+                return Color.GREEN;
+            case 5:
+                return Color.BLUE;
+            case 6:
+                return Color.argb(272, 137, 0, 255);
+            case 7:
+                return Color.BLACK;
+            default:
+                return Color.WHITE;
+        }
+    }
 
     void StartRandomAndTimer(){
         random = new Random();
@@ -87,26 +132,26 @@ public class GameActivity extends ActionBarActivity {
         timerTask = new TimerTask() {
             @Override
             public void run() {
-                timerhandler.post(new Runnable() {
+                timerHandler.post(new Runnable() {
                     @Override
                     public void run() {
                         randomNumber = 1 + random.nextInt(4);
                         switch(randomNumber){
                             case 1:
-                                button2.setEnabled(true);
-                                button2.setBackgroundColor(Color.RED);
+                                button[0][0].setEnabled(true);
+                                button[0][0].setBackgroundColor(Color.RED);
                                 break;
                             case 2:
-                                button3.setEnabled(true);
-                                button3.setBackgroundColor(Color.RED);
+                                button[0][1].setEnabled(true);
+                                button[0][1].setBackgroundColor(Color.RED);
                                 break;
                             case 3:
-                                button4.setEnabled(true);
-                                button4.setBackgroundColor(Color.RED);
+                                button[1][0].setEnabled(true);
+                                button[1][0].setBackgroundColor(Color.RED);
                                 break;
                             case 4:
-                                button5.setEnabled(true);
-                                button5.setBackgroundColor(Color.RED);
+                                button[1][1].setEnabled(true);
+                                button[1][1].setBackgroundColor(Color.RED);
                                 break;
                         }
                     }
